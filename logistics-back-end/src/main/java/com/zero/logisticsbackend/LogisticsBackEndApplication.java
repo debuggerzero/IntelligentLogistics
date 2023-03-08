0efdf658ddbbfbd7d184fc97d8b8f6287111cc98
zero <debuggerzero@gmail.com>
2023-03-08 22:16:48 +0800

    初始化项目

---
+++ logistics-back-end/src/main/java/com/zero/logisticsbackend/LogisticsBackEndApplication.java
@@ -0,0 +1,16 @@
+package com.zero.logisticsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ZERO
+ */
+@SpringBootApplication
+public class LogisticsBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogisticsBackEndApplication.class, args);
+    }
+
+}
---
+++ logistics-back-end/src/test/java/com/zero/logisticsbackend/LogisticsBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero.logisticsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
