583d2e5eec9897979ac01cb6b2e7903eca2a3c93
zero <debuggerzero@gmail.com>
2023-03-12 17:32:30 +0800

    添加 mybatis-plus 配置类

---
+++ logistics-back-end/src/main/java/com/zero/logisticsbackend/config/MybatisPlusConfig.java
@@ -0,0 +1,13 @@
+package com.zero.logisticsbackend.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MybatisPlus 配置类
+ *
+ * @author ZERO
+ * @date 2023/3/8
+ */
+@Configuration
+public class MybatisPlusConfig {
+}
